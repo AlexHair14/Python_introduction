6bbaff46c7bdad2e8580932da9572d1d0c05e39a
Alex Hair <78123278+AlexHair14@users.noreply.github.com>
2023-04-14 12:07:54 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -8,3 +8,11 @@ while x < 5:
     print("hello world")
     #adds 1 to x value so not to cause an infinite loop
     x += 1 
+
+print("\n")
+
+for i in range(5):
+    time.sleep(0.5)
+    print("hello world")
+    #this prints hello world exactly 5 times and doesn't need a condition to be broken to stop 
+    #it will only go for the specified number of times
